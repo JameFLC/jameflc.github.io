4ab2209c26933e0de096e6a62aca94a8df936694
Jame Floc'h Le Carour <81566176+JameFLC@users.noreply.github.com>
2025-08-30 00:26:57 +0200

    Added twitter meta tags to blog

--- src/routes/(blog)/en/blog/[slug]/+page.svelte
+++ src/routes/(blog)/en/blog/[slug]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import { websiteURL } from '$lib';
 	import BlogHeader from '$lib/components/blog/BlogHeader.svelte';
 
@@ -11,6 +12,13 @@
 	<meta property="og:title" content={data.meta.title} />
 	<meta property="og:description" content={data.meta.description} />
 	<meta property="og:image" content={websiteURL + data.meta.thumbnail} />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content={websiteURL} />
+	<meta property="twitter:url" content={websiteURL + page.url.pathname} />
+	<meta name="twitter:title" content={data.meta.title} />
+	<meta name="twitter:description" content={data.meta.description} />
+	<meta name="twitter:image" content={websiteURL + data.meta.thumbnail} />
 </svelte:head>
 
 <article>
